75763079599c3ce8ced0a13aab9c83004fcb2af8
ming-90 <minki319@naver.com>
2023-09-05 23:40:57 +0900

    chapter 3

---
+++ chapter_2/3_neural.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+###############
+# step function
+###############
+def step_function(x):
+    return np.array(x > 0, dtype=int)
+
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = step_function(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+###############
+# sigmoid function
+###############
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
