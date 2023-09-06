16a6aa3e65a0eeb5431e4828ce1ede529337a3db
ming-90 <minki319@naver.com>
2023-09-06 23:46:56 +0900

    study

---
+++ chapter/4_train.py
@@ -0,0 +1,84 @@
+import numpy as np
+import os, sys
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+#################
+# sum of squares for error, SSE
+#################
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+t = [0,0,1,0,0,0,0,0,0,0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+# test = sum_squares_error(np.array(y), np.array(t))
+
+# print(test)
+
+#################
+# cross entropy error, CEE
+#################
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return - np.sum(t * np.log(y + delta))
+
+# test = cross_entropy_error(np.array(y), np.array(t))
+# print(test)
+
+
+#################
+# mini batch
+#################
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize=True, one_hot_label=True)
+    return x_test, t_test
+# get_data()
+
+#################
+# Numerical diff - 수치미분
+#################
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+#################
+# Numerical gradient
+#################
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x) # x와 형상이 같은 배열 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+#################
+# Gradient descent
+#################
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
