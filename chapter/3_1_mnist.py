6d8ee369a96b60062f163478b7bf4dda12cf95e6
ming-90 <minki319@naver.com>
2023-09-06 16:39:18 +0900

    study

--- chapter/3_1_mnist.py
+++ chapter/3_1_mnist.py
@@ -46,10 +46,11 @@ x, t = get_data()
 network = init_network()
 
 accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
-    if p == t[i]:
-        accuracy_cnt += 1
+batch_size = 100
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
 print(f"Accuracy: {float(accuracy_cnt) / len(x)}")
